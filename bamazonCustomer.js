e4457a0b7a0c11fafc8d8f315951b99c364bd53c
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-30 18:13:59 -0400

    connected

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,17 +2,70 @@ require('dotenv').config();
 const key = require('./key.js');
 const mySQL = require("mysql");
 const inquirer = require("inquirer");
+let inventory = [];
 // const { table } = require('table');
 
 // console.log('hi' + mySQL);
+// console.log(key.mySQL.userName);
 var connection = mySQL.createConnection({
     host: "localhost",
     port: 3306,
-    user: key.mySQL.user,
+    user: key.mySQL.userName,
     password: key.mySQL.password,
-    database: "bamazon"
+    database: "bamazon",
+    insecureAuth : true
 });
-// console.log(connection);
+
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    // console.log("this is working");
+    
+    start();
+});
+
+// console.log(makeTable);
+
+function start() {
+    inquirer
+      .prompt({
+        name: "shopping",
+        type: "list",
+        message: "Would you like to buy [Coffee], an [Espresso Grinder] or a [Regular Grinder]?",
+        choices: ["Coffee", "Espresso Grinder", "Regular Grinder", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the coffee or espressoGrinder or regularGrinder functions
+        if (answer.shipping === "Coffee") {
+        //   coffee();
+        }
+        else if(answer.shopping === "Espresso Grinder") {
+        //   espressoGrinder();
+        } else if(answer.shopping === "Regular Grinder") {
+        //   regularGrinder();
+        } else {
+          connection.end();
+        }
+      });
+}
+
+// function coffee() {
+//     connection.query("SELECT department_name * FROM products", function (err, results) {
+//         if (err) throw err;
+//     }
+        
+        
+// }
+
+
+// function makeTable() {
+//     connection.query("SELECT * FROM products", function (err, results) {
+//         if (err) throw err;
+    
+
+//     })
 
  
 
