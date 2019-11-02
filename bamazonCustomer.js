d582be66cf400e2dea724a28e2371693d78c4ecd
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-01 22:07:16 -0400

    added more options

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,10 +43,9 @@ function questions() {
           coffee();
         }
         else if (answer.shopping === "Espresso Grinder") {
-          // coffee();
-          // espressoGrinder();
+          espressoGrinder();
         } else if(answer.shopping === "Regular Grinder") {
-        //   regularGrinder();
+          regularGrinder();
         } else {
           connection.end();
         }
@@ -56,6 +55,86 @@ function questions() {
 function coffee() {
 
   connection.query("SELECT * FROM products WHERE department_name=?", ["Coffee"], function(err, res) {
+    
+    if (err) throw err;
+    console.table(res);
+
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "item_id",
+        message: "Which Item Id would you like to buy?"
+      },
+      {
+        type: "input",
+        name: "stock_quantity",
+        message: "How much were you looking to buy?"
+      }
+    ]).then(function (userInput) {
+        
+      var stock_quantity = inventory[userInput.item_id -1].stock_quantity;
+      var price = inventory[userInput.item_id -1].price;
+  
+      // console.log(stock_quantity);
+       
+      if (stock_quantity > userInput.stock_quantity) {
+        var customerTotal = price * userInput.stock_quantity;
+        console.log("Bro your order will be: $" + customerTotal);
+        // customerTotal(price, userInput.stock_quantity, stock_quantity);
+        connection.end();
+      } else {
+        console.log("Not enough in stock");
+        connection.end();
+      }
+    })
+      
+  });
+
+}
+
+function espressoGrinder() {
+
+  connection.query("SELECT * FROM products WHERE department_name=?", ["Espresso Grinder"], function(err, res) {
+    
+    if (err) throw err;
+    console.table(res);
+
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "item_id",
+        message: "Which Item Id would you like to buy?"
+      },
+      {
+        type: "input",
+        name: "stock_quantity",
+        message: "How much were you looking to buy?"
+      }
+    ]).then(function (userInput) {
+        
+      var stock_quantity = inventory[userInput.item_id -1].stock_quantity;
+      var price = inventory[userInput.item_id -1].price;
+  
+      // console.log(stock_quantity);
+       
+      if (stock_quantity > userInput.stock_quantity) {
+        var customerTotal = price * userInput.stock_quantity;
+        console.log("Bro your order will be: $" + customerTotal);
+        // customerTotal(price, userInput.stock_quantity, stock_quantity);
+        connection.end();
+      } else {
+        console.log("Not enough in stock");
+        connection.end();
+      }
+    })
+      
+  });
+
+}
+
+function regularGrinder() {
+
+  connection.query("SELECT * FROM products WHERE department_name=?", ["Regular Grinder"], function(err, res) {
     
     if (err) throw err;
     console.table(res);
